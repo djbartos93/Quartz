ef5b2f38f97e6132b3850b1b8d1b1cbb679ad3fd
Joel Yauk <jbyauk@gmail.com>
2018-10-20 20:43:41 -0500

    Redirect to player edit after first successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def after_sign_up_path_for(resource)
-    if resource.is_a?(User)
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.sign_in_count == 1
       player = Player.find_by user_id: resource.id
       return edit_player_path(player)
     else
