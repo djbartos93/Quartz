705ca82b3609d6fa0b0a15dbacc37a42361a04dd
Eric Bartos <djbartos93@gmail.com>
2018-10-29 22:34:16 -0500

    backup codes on 2fa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   def disable_otp
     current_user.otp_required_for_login = false
     current_user.save!
-    redirect_to home_index_path
+    redirect_to users_edit_path
   end
 
   def enable_otp
     current_user.otp_secret = User.generate_otp_secret
     current_user.otp_required_for_login = true
+    codes = current_user.generate_otp_backup_codes!
     current_user.save!
-    redirect_to home_index_path
+    redirect_to users_edit_path
   end
 end
---
+++ db/migrate/20181030020139_add_devise_two_factor_bacupable_to_users.rb
@@ -0,0 +1,6 @@
+class AddDeviseTwoFactorBacupableToUsers < ActiveRecord::Migration[5.2]
+  def change
+    # Change type from :string to :text if using MySQL database
+    add_column :users, :otp_backup_codes, :string, array: true
+  end
+end
